72c5b9cb446119d6e227e5b0a8d8fad90c351b01
paulaanb <pnarabor@myuax.com>
2023-11-25 20:27:28 +0100

    s

--- myproject/myapp/manage.py
+++ myproject/myapp/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.web.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
+
 
     try:
         from django.core.management import execute_from_command_line
