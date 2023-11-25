b2cc4b73c6e762ccbda70c0f5d5e69ded389c7b4
paulaanb <pnarabor@myuax.com>
2023-11-25 18:49:46 +0100

    no se si funciona

--- myproject/builderpattern/pizza.py
+++
@@ -1,9 +0,0 @@
-class Pizza():
-    def __init__(self):
-        self.masa= ''
-        self.salsa= ''
-        self.ingredientes_principales= []
-        self.coccion= [] 
-        self.presentacion= ''
-        self.maridaje_recomendado= ''
-        self.extra= []
